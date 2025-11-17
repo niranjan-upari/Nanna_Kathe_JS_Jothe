a886d672627024805112afa28aa0973c4d83b104
niranjan <niranjan.upari@gmail.com>
2025-11-17 22:27:52 +0530

    commiting variables

--- cls1_hello.js
+++ cls1_hello.js
@@ -1,1 +1,8 @@
 console.log("Om Ganeshaya Namaha! Welcome to JavaScript")
+
+
+let Firstname = "Niranjan "
+const lastname = "Upari"
+console.log(Firstname + lastname)
+var city = "Pune"
+console.log(city)
