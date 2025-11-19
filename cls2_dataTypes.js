93592f80e0df5ac2f51f81a4a0f98281ce84207a
niranjan <niranjan.upari@gmail.com>
2025-11-20 00:06:18 +0530

    String dataType is committed

--- cls2_dataTypes.js
+++ cls2_dataTypes.js
@@ -1,4 +1,4 @@
-// 1.Numbers - For any kind of numbers
+console.log("1. Numbers - For any kind of numbers")
 
 let a = 33
 console.log(typeof a)
@@ -10,4 +10,12 @@ let c = 1/0
 console.log(c)
 console.log(typeof c)
 
-// 2. String -Must be surrounded by quotes
+console.log("2. String -Must be surrounded by quotes")
+
+let str = "good"
+let str1 = 'better'
+let str2 = `best ${"man"}`
+
+console.log(str)
+console.log(str1)
+console.log(str2)
