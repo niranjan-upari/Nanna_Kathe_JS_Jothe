b81f4d6d1487f07df3a8c44a30d3156ac7da9c7c
niranjan <niranjan.upari@gmail.com>
2025-11-19 23:59:01 +0530

    Number dataType is committed

---
+++ cls2_dataTypes.js
@@ -0,0 +1,13 @@
+// 1.Numbers - For any kind of numbers
+
+let a = 33
+console.log(typeof a)
+
+const b = 11.11
+console.log(typeof b)
+
+let c = 1/0
+console.log(c)
+console.log(typeof c)
+
+// 2. String -Must be surrounded by quotes
